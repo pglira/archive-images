90678661a54557a69ce78d84e9740e59187fd0d0
Philipp Glira <philipp.glira@ait.ac.at>
2021-12-18 13:04:13 +0000

    Autoformat

--- archiveImages/archive_images.py
+++ archiveImages/archive_images.py
@@ -16,14 +16,20 @@ def main(args_in=sys.argv[1:]):
 
     for idx_image, image_source in enumerate(images_source):
         timestamp = get_timestamp(image_source)
-        image_target = get_image_target(args, image_source, timestamp, idx_image, len(images_source))
+        image_target = get_image_target(
+            args, image_source, timestamp, idx_image, len(images_source)
+        )
         if image_target is not None:
-            image_target = check_duplicate(args, image_source, image_target, idx_image, len(images_source))
+            image_target = check_duplicate(
+                args, image_source, image_target, idx_image, len(images_source)
+            )
         if image_target is not None:
             image_target = add_suffix(image_target)
-            add_image_to_archive(args, image_source, image_target, idx_image, len(images_source))
+            add_image_to_archive(
+                args, image_source, image_target, idx_image, len(images_source)
+            )
 
-    logging.info('Finished!')
+    logging.info("Finished!")
     sys.exit(0)
 
 
@@ -38,26 +44,73 @@ def parse_args(args_in):
         return path
 
     def image_extensions(input_string):
-        return input_string.split(',')
+        return input_string.split(",")
 
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser._action_groups.pop()
-    required = parser.add_argument_group('required arguments')
-    optional = parser.add_argument_group('optional arguments')
-    required.add_argument('-i', '--imageFolder', help='Path to folder with images to archive', dest='imageFolder',
-                          required=True, type=path_to_folder)
-    required.add_argument('-a', '--imageArchive', help='Path to folder with image archive', dest='imageArchive',
-                          required=True, type=path_to_folder)
-    optional.add_argument('-e', '--imageExtensions', help='Extensions of images to archive separated by commas',
-                          dest='imageExtensions', required=False, default=['jpg', 'jpeg'], type=image_extensions)
-    optional.add_argument('-m', '--mode', help='Move or copy image files to archive?', dest='mode',
-                          required=False, default='copy', choices=['copy', 'move'])
-    optional.add_argument('-d', '--addDuplicates', help='Add duplicates to a subfolder "duplicates" in image archive?',
-                          dest='addDuplicates', required=False, action='store_true')
-    optional.add_argument('-n', '--addNoExif', help='Add images with no exif information to a subfolder "no_exif" in \
-                          image archive?', dest='addNoExif', required=False, action='store_true')
-    optional.add_argument('-c', '--confirm', help='Confirm each operation before execution?', dest='confirm',
-                          required=False, action='store_true')
+    required = parser.add_argument_group("required arguments")
+    optional = parser.add_argument_group("optional arguments")
+    required.add_argument(
+        "-i",
+        "--imageFolder",
+        help="Path to folder with images to archive",
+        dest="imageFolder",
+        required=True,
+        type=path_to_folder,
+    )
+    required.add_argument(
+        "-a",
+        "--imageArchive",
+        help="Path to folder with image archive",
+        dest="imageArchive",
+        required=True,
+        type=path_to_folder,
+    )
+    optional.add_argument(
+        "-e",
+        "--imageExtensions",
+        help="Extensions of images to archive separated by commas",
+        dest="imageExtensions",
+        required=False,
+        default=["jpg", "jpeg"],
+        type=image_extensions,
+    )
+    optional.add_argument(
+        "-m",
+        "--mode",
+        help="Move or copy image files to archive?",
+        dest="mode",
+        required=False,
+        default="copy",
+        choices=["copy", "move"],
+    )
+    optional.add_argument(
+        "-d",
+        "--addDuplicates",
+        help='Add duplicates to a subfolder "duplicates" in image archive?',
+        dest="addDuplicates",
+        required=False,
+        action="store_true",
+    )
+    optional.add_argument(
+        "-n",
+        "--addNoExif",
+        help='Add images with no exif information to a subfolder "no_exif" in \
+                          image archive?',
+        dest="addNoExif",
+        required=False,
+        action="store_true",
+    )
+    optional.add_argument(
+        "-c",
+        "--confirm",
+        help="Confirm each operation before execution?",
+        dest="confirm",
+        required=False,
+        action="store_true",
+    )
     args = parser.parse_args(args_in)
 
     if args.imageFolder == args.imageArchive:
@@ -69,10 +122,10 @@ def parse_args(args_in):
 
 def setup_logging(args_in, args):
     """Set up and start logger."""
-    output_dir = os.path.join(args.imageArchive, 'logs')
+    output_dir = os.path.join(args.imageArchive, "logs")
     os.makedirs(output_dir, exist_ok=True)
     now = datetime.datetime.now()
-    file = 'log_' + now.strftime('%Y-%m-%d_%H-%M-%S') + '.txt'
+    file = "log_" + now.strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
     path_to_log = os.path.join(output_dir, file)
 
     # Create logger
@@ -80,13 +133,13 @@ def setup_logging(args_in, args):
     logger.setLevel(logging.INFO)
 
     # Create file handler
-    fh = logging.FileHandler(path_to_log, mode='w')
+    fh = logging.FileHandler(path_to_log, mode="w")
 
     # Create console handler
     ch = logging.StreamHandler(sys.stdout)
 
     # Create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s|%(levelname)s: %(message)s')
+    formatter = logging.Formatter("%(asctime)s|%(levelname)s: %(message)s")
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
 
@@ -95,14 +148,16 @@ def setup_logging(args_in, args):
     logger.addHandler(ch)
 
     # Start and report input arguments
-    logging.info('Arguments: ' + ' '.join(args_in))
+    logging.info("Arguments: " + " ".join(args_in))
 
 
 def find_images(args):
     """Find all images recursively."""
     images_source = []
     for ext in args.imageExtensions:
-        images_to_add = glob.glob(os.path.join(args.imageFolder, '**/*.' + ext), recursive=True)
+        images_to_add = glob.glob(
+            os.path.join(args.imageFolder, "**/*." + ext), recursive=True
+        )
         if images_to_add:
             images_source.extend(images_to_add)
     if not images_source:
@@ -119,21 +174,22 @@ def get_timestamp(image):
     #   date and time of when images was digitized, e.g. scanning of an image
     # DateTime (306), SubsecTime (37520):
     #   date and time of when image file was created or last edited
-    exif_tags = [(36867, 37521),
-                 (36868, 37522),
-                 (306, 37520)]
+    exif_tags = [(36867, 37521), (36868, 37522), (306, 37520)]
     exif = read_exif(image)
     if exif is not None:
         for tag in exif_tags:
             datetime_exif = exif.get(tag[0])
-            subseconds_exif = int(exif.get(tag[1], 0)) / 1000  # subseconds are milliseconds
+            subseconds_exif = (
+                int(exif.get(tag[1], 0)) / 1000
+            )  # subseconds are milliseconds
             if datetime_exif is not None:  # stop if timestamp was found in exif tag
                 break
         if datetime_exif is None:
             return None
         else:
-            timestamp = datetime.datetime.strptime(datetime_exif, '%Y:%m:%d %H:%M:%S') + \
-                        datetime.timedelta(0, subseconds_exif)
+            timestamp = datetime.datetime.strptime(
+                datetime_exif, "%Y:%m:%d %H:%M:%S"
+            ) + datetime.timedelta(0, subseconds_exif)
             return timestamp
     else:  # image has no exif data
         return None
@@ -146,8 +202,10 @@ def read_exif(image):
     except IOError:
         logging.error('"{}" can not be opened as image!'.format(image))
         sys.exit(1)
-    get_exif = getattr(img, '_getexif', None)
-    if callable(get_exif):  # if instance 'img' has method 'get_exif' (e.g. not true for png files)
+    get_exif = getattr(img, "_getexif", None)
+    if callable(
+        get_exif
+    ):  # if instance 'img' has method 'get_exif' (e.g. not true for png files)
         exif = img._getexif()
     else:
         exif = None
@@ -157,17 +215,24 @@ def read_exif(image):
 def get_image_target(args, image_source, timestamp, idx_image, num_images):
     """Get target filename of image in image archive."""
     if timestamp is not None:
-        year = timestamp.strftime('%Y')
-        month = timestamp.strftime('%m')
-        file = timestamp.strftime('%Y_%m_%d_%H_%M_%S.%f')[:-3]  # %f gives microseconds, but we only want milliseconds
+        year = timestamp.strftime("%Y")
+        month = timestamp.strftime("%m")
+        file = timestamp.strftime("%Y_%m_%d_%H_%M_%S.%f")[
+            :-3
+        ]  # %f gives microseconds, but we only want milliseconds
         ext = os.path.splitext(image_source)[1]
-        image_target = os.path.join(args.imageArchive, year, month, file + ext)
+        image_target = os.path.join(args.imageArchive, year, month, file + ext.lower())
     else:
         if args.addNoExif:
-            image_target = os.path.join(args.imageArchive, 'no_exif_data', os.path.basename(image_source))
+            image_target = os.path.join(
+                args.imageArchive, "no_exif_data", os.path.basename(image_source)
+            )
         else:
-            logging.warning('Image {} of {}: skip "{}" due to missing exif data!'.format(idx_image + 1, num_images,
-                                                                                         image_source))
+            logging.warning(
+                'Image {} of {}: skip "{}" due to missing exif data!'.format(
+                    idx_image + 1, num_images, image_source
+                )
+            )
             image_target = None
     return image_target
 
@@ -175,15 +240,30 @@ def get_image_target(args, image_source, timestamp, idx_image, num_images):
 def check_duplicate(args, image_source, image_target, idx_image, num_images):
     """Check if a duplicate image exists already in image archive and alter image_target if necessary."""
     if os.path.isdir(os.path.dirname(image_target)):
-        files_to_check = [name for name in os.listdir(os.path.dirname(image_target))
-                          if name.startswith(os.path.splitext(os.path.basename(image_target))[0])]
+        files_to_check = [
+            name
+            for name in os.listdir(os.path.dirname(image_target))
+            if name.startswith(os.path.splitext(os.path.basename(image_target))[0])
+        ]
         for file in files_to_check:
-            if isduplicate(image_source, os.path.join(os.path.dirname(image_target), file)):
+            if isduplicate(
+                image_source, os.path.join(os.path.dirname(image_target), file)
+            ):
                 if args.addDuplicates:
-                    image_target = os.path.join(args.imageArchive, 'duplicate_images', os.path.basename(image_target))
+                    image_target = os.path.join(
+                        args.imageArchive,
+                        "duplicate_images",
+                        os.path.basename(image_target),
+                    )
                 else:
-                    logging.warning('Image {} of {}: skip "{}" as it is a duplicate of "{}"!'.format(idx_image + 1,
-                                    num_images, image_source, os.path.join(os.path.dirname(image_target), file)))
+                    logging.warning(
+                        'Image {} of {}: skip "{}" as it is a duplicate of "{}"!'.format(
+                            idx_image + 1,
+                            num_images,
+                            image_source,
+                            os.path.join(os.path.dirname(image_target), file),
+                        )
+                    )
                     image_target = None
                 return image_target
     return image_target
@@ -191,7 +271,7 @@ def check_duplicate(args, image_source, image_target, idx_image, num_images):
 
 def isduplicate(image1, image2):
     """Check if two image files are identical."""
-    return open(image1, 'rb').read() == open(image2, 'rb').read()
+    return open(image1, "rb").read() == open(image2, "rb").read()
 
 
 def add_suffix(image_target):
@@ -200,22 +280,27 @@ def add_suffix(image_target):
     image_target_orig = image_target
     i_suffix = 1
     while os.path.exists(image_target):
-        image_target = image_target_orig.replace(ext, '_{:02d}'.format(i_suffix) + ext)
+        image_target = image_target_orig.replace(ext, "_{:02d}".format(i_suffix) + ext)
         i_suffix += 1
     return image_target
 
 
 def add_image_to_archive(args, image_source, image_target, idx_image, num_images):
     """Adds image to image archive."""
-    logging.info('Image {} of {}: {} "{}" to "{}"'.format(idx_image + 1, num_images,
-                                                          args.mode, image_source, image_target))
+    logging.info(
+        'Image {} of {}: {} "{}" to "{}"'.format(
+            idx_image + 1, num_images, args.mode, image_source, image_target
+        )
+    )
     if args.confirm:
-        confirm = input('{} "{}" to "{}"? (y/n) '.format(args.mode, image_source, image_target))
-        if confirm == 'n':
+        confirm = input(
+            '{} "{}" to "{}"? (y/n) '.format(args.mode, image_source, image_target)
+        )
+        if confirm == "n":
             return
     os.makedirs(os.path.dirname(image_target), exist_ok=True)
     getattr(shutil, args.mode)(image_source, image_target)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
-required_packages = ['Pillow']
+required_packages = ["Pillow"]
 
 setup(
-    name='archiveImages',
-    version='0.1',
-    description='Archive images based on exif timestamp.',
-    author='Philipp Glira',
-    author_email='philipp.glira@gmail.com',
+    name="archiveImages",
+    version="0.1",
+    description="Archive images based on exif timestamp.",
+    author="Philipp Glira",
+    author_email="philipp.glira@gmail.com",
     classifiers=[
-        'Natural Language :: English',
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3.6',
+        "Natural Language :: English",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.6",
     ],
     packages=find_packages(),
     install_requires=required_packages,
     entry_points={
-        'console_scripts': [
-            'archiveImages=archiveImages.archive_images:main',
+        "console_scripts": [
+            "archiveImages=archiveImages.archive_images:main",
         ],
     },
 )
